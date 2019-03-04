2487c9fa21f4b8301b28db8c665f09d348795e38
Tran Trung Tinh <TranTrungTinh@TinhBookPro.local>
2019-03-04 11:24:10 +0700

    css review how to use

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ $('#nav-intro').click(e => {
   if (!hash) return;
     $('html, body').animate({ scrollTop: $(hash).offset().top }, 800);
 });
+
+var introFirst = document.getElementById('content-intro-01');
+var introSecond = document.getElementById('content-intro-02');
+
+lottie.loadAnimation({
+  container: introFirst, // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: './animations/shasket_landingpage_animation_01_vie.json' // the path to the animation json
+});
+
+lottie.loadAnimation({
+  container: introSecond, // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: './animations/shasket_landingpage_animation_02.json' // the path to the animation json
+});
